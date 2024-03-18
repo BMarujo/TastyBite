48fd0ae8d0dff7300e3b5dbb9906e06274c02047
Bernardo Marujo <bernardomarujo@ua.pt>
2024-03-18 01:07:19 +0000

    Update import statement for Message class

--- lib/chat_service/chat_services.dart
+++ lib/chat_service/chat_services.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:tastybite/message_model/message.dart';
+import 'package:tastybite/util/message_model/message.dart';
 
 class ChatService {
   final FirebaseFirestore database;
