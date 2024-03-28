ae6938e3ce9b56d36ca1dfec4d1a8677f9350b17
Bernardo Marujo <bernardomarujo@ua.pt>
2024-03-28 22:57:22 +0000

    falta uma cena

--- lib/home_screens/home_screen/history.dart
+++ lib/home_screens/home_screen/history.dart
@@ -13,56 +13,91 @@ class HistoryPage extends StatelessWidget {
           'Histórico de Compras',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor:
-            Colors.blue, // Set the app bar background color to blue
+        backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(
-        itemCount: user.getHistory.length,
-        itemBuilder: (context, index) {
-          String historyItem = user.getHistory[index];
-          String dateItem = user.getDate[index];
-          Key itemkey = Key(historyItem);
-          return Card(
-            color: Colors.blue.shade200,
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Dismissible(
-              key: itemkey,
-              direction: DismissDirection.startToEnd,
-              onDismissed: (direction) {
-                user.removeHistory(historyItem);
-                user.removeDate(dateItem);
+      body: FutureBuilder<List<String>>(
+        future: user.getHistory(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error: ${snapshot.error}',
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            );
+          } else {
+            List<String> history = snapshot.data ?? [];
+            return ListView.builder(
+              itemCount: history.length,
+              itemBuilder: (context, index) {
+                String historyItem = history[index];
+                return FutureBuilder<List<String>>(
+                  future: user.getDate(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text(
+                        'Error: ${snapshot.error}',
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      );
+                    } else {
+                      List<String> dates = snapshot.data ?? [];
+                      String dateItem = dates[index];
+                      Key itemKey = Key(historyItem);
+                      return Card(
+                        color: Colors.blue.shade200,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        child: Dismissible(
+                          key: itemKey,
+                          direction: DismissDirection.startToEnd,
+                          onDismissed: (direction) {
+                            user.removeHistory(historyItem);
+                            user.removeDate(dateItem);
+                          },
+                          background: Container(
+                            color: Colors.red,
+                            alignment: Alignment.centerLeft,
+                            padding: const EdgeInsets.only(left: 20),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  'Remover',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          child: ListTile(
+                            subtitle: Text(
+                              'Data da Compra: $dateItem',
+                              style: const TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 17),
+                            ),
+                            title: Text(
+                              historyItem,
+                              style: const TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 25),
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                );
               },
-              background: Container(
-                color: Colors.red,
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.only(left: 20),
-                child: const Row(
-                  children: [
-                    Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      'Remover',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-              child: ListTile(
-                subtitle: Text(
-                  'Data da Compra: $dateItem',
-                  style: const TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 17),
-                ),
-                title: Text(
-                  historyItem,
-                  style: const TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 25),
-                ),
-              ),
-            ),
-          );
+            );
+          }
         },
       ),
     );
--- lib/util/myuser.dart
+++ lib/util/myuser.dart
@@ -1,31 +1,100 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tastybite/locator/service_locator.dart';
+
+final FirebaseAuth _auth = locator.get();
+final FirebaseFirestore _firestore = locator.get();
+
 class MyUser {
   final String? name;
-
-  List<String> history = [];
-
-  List<String> date = [];
 
   MyUser({this.name});
 
   String? get getname => name;
 
-  List<String> get getHistory => history;
+  Future<List<String>> getHistory() async {
+    try {
+      final DocumentSnapshot snapshot = await _firestore
+          .collection('Users')
+          .doc(_auth.currentUser!.uid)
+          .get();
 
-  List<String> get getDate => date;
+      final data = snapshot.data() as Map<String, dynamic>?;
 
-  void addHistory(String item) {
-    history.add(item);
+      if (data == null || !data.containsKey('history')) {
+        throw Exception('History data not available');
+      }
+
+      return List<String>.from(data['history']);
+    } catch (e) {
+      // Handle errors, e.g., logging or notifying the user
+      print('Error retrieving history: $e');
+      return [];
+    }
   }
 
-  void removeHistory(String item) {
-    history.remove(item);
+  Future<List<String>> getDate() async {
+    try {
+      final DocumentSnapshot snapshot = await _firestore
+          .collection('Users')
+          .doc(_auth.currentUser!.uid)
+          .get();
+
+      final data = snapshot.data() as Map<String, dynamic>?;
+
+      if (data == null || !data.containsKey('date')) {
+        throw Exception('Date data not available');
+      }
+
+      return List<String>.from(data['date']);
+    } catch (e) {
+      // Handle errors, e.g., logging or notifying the user
+      print('Error retrieving date: $e');
+      return [];
+    }
   }
 
-  void addDate(String item) {
-    date.add(item);
+  Future<void> addHistory(String item) async {
+    try {
+      await _firestore.collection('Users').doc(_auth.currentUser!.uid).update({
+        'history': FieldValue.arrayUnion([item])
+      });
+    } catch (e) {
+      // Handle errors, e.g., logging or notifying the user
+      print('Error adding history: $e');
+    }
   }
 
-  void removeDate(String item) {
-    date.remove(item);
+  Future<void> removeHistory(String item) async {
+    try {
+      await _firestore.collection('Users').doc(_auth.currentUser!.uid).update({
+        'history': FieldValue.arrayRemove([item])
+      });
+    } catch (e) {
+      // Handle errors, e.g., logging or notifying the user
+      print('Error removing history: $e');
+    }
+  }
+
+  Future<void> addDate(String item) async {
+    try {
+      await _firestore.collection('Users').doc(_auth.currentUser!.uid).update({
+        'date': FieldValue.arrayUnion([item])
+      });
+    } catch (e) {
+      // Handle errors, e.g., logging or notifying the user
+      print('Error adding date: $e');
+    }
+  }
+
+  Future<void> removeDate(String item) async {
+    try {
+      await _firestore.collection('Users').doc(_auth.currentUser!.uid).update({
+        'date': FieldValue.arrayRemove([item])
+      });
+    } catch (e) {
+      // Handle errors, e.g., logging or notifying the user
+      print('Error removing date: $e');
+    }
   }
 }
